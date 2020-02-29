da3cb023d88eb85425df1f03d1ce7cd44b3c15ca
ruifchaves <ruichaves99@gmail.com>
2020-02-29 13:24:56 +0000

    Added commented help in OlaMundo project

--- Ficha01/IntelliJ/src/OlaMundo.java
+++ Ficha01/IntelliJ/src/OlaMundo.java
@@ -1,2 +1,20 @@
+
+/*
+Utilizando a linha de comando
+        Compilação: $javac OlaMundo.java
+        • javac é o compilador de Java para bytecode
+        • Se correu bem (ie. sem erros) foi gerado o cheiro OláMundo.class
+Execução: $java OláMundo
+        • java é a máquina virtual Java
+        • não se deve indicar a extensão do cheiro (.class), apenas o nome da classe
+
+INTELLIJ: Open the class in the editor and do one of the following:
+    -In the gutter, clickRun icon, and choose the desired command.
+    -Choose Run <method name > from the context menu
+    -Press Ctrl+Shift+F10
+*/
 public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+    }
 }
